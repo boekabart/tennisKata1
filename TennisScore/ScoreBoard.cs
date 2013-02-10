8fb36fa153b2b0b2da4ee99c65809c9f57f81efd
Boekabart <boekabart@gmail.com>
2013-02-10 23:07:46 +0100

    Passed test (if)

--- TennisScore/ScoreBoard.cs
+++ TennisScore/ScoreBoard.cs
@@ -9,15 +9,17 @@ namespace TennisScore
     public class ScoreBoard
     {
         private readonly Game m_Game;
+        private readonly string m_Player1Name;
 
         public ScoreBoard(Game game, string player1Name, string player2Name)
         {
             m_Game = game;
+            m_Player1Name = player1Name;
         }
 
         public string DisplayString
         {
-            get { return m_Game.ScoreString; }
+            get { return m_Game.HasWinner ? string.Format("{0} wins", m_Player1Name) : m_Game.ScoreString; }
         }
     }
 }
