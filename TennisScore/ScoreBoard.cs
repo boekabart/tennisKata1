16659521c3403e6bcaaffa01458597d43a5e2fb0
Boekabart <boekabart@gmail.com>
2013-02-10 23:09:41 +0100

    Passed test (scalar=>array)

--- TennisScore/ScoreBoard.cs
+++ TennisScore/ScoreBoard.cs
@@ -9,17 +9,22 @@ namespace TennisScore
     public class ScoreBoard
     {
         private readonly Game m_Game;
-        private readonly string m_Player1Name;
+        private readonly string[] m_PlayerNames;
 
         public ScoreBoard(Game game, string player1Name, string player2Name)
         {
             m_Game = game;
-            m_Player1Name = player1Name;
+            m_PlayerNames = new[] {player1Name, player2Name};
         }
 
         public string DisplayString
         {
-            get { return m_Game.HasWinner ? string.Format("{0} wins", m_Player1Name) : m_Game.ScoreString; }
+            get
+            {
+                return m_Game.HasWinner
+                           ? string.Format("{0} wins", m_PlayerNames[m_Game.Winner - 1])
+                           : m_Game.ScoreString;
+            }
         }
     }
 }
