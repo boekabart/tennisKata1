9c0b9c8112ab525c94d18676c3aab3f5fdf164c8
Boekabart <boekabart@gmail.com>
2013-02-10 22:58:13 +0100

    Added ScoreBoard class, with constructor test

---
+++ TennisScore/ScoreBoard.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TennisScore
+{
+    public class ScoreBoard
+    {
+        public ScoreBoard(Game game)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TennisScoreTest/ScoreBoardTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TennisScore;
+
+namespace TennisScoreTest
+{
+    [TestFixture]
+    class ScoreBoardTest
+    {
+        private Game m_Game;
+
+        [SetUp]
+        public void SetUp()
+        {
+            m_Game = new Game();
+        }
+
+        [Test]
+        public void ScoreBoardClassTest()
+        {
+            var scoreBoard = new ScoreBoard(m_Game);
+        }
+    }
+}
