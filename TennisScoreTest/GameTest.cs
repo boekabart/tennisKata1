682f8b53da2a251ce4611d841e50ac0063d4a640
Boekabart <boekabart@gmail.com>
2013-02-10 21:26:12 +0100

    Created Test: NewGame_NoWinner

--- TennisScoreTest/GameTest.cs
+++ TennisScoreTest/GameTest.cs
@@ -4,10 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using TennisScore;
 
 namespace TennisScoreTest
 {
     public class GameTest
     {
+        [Test]
+        void NewGame_NoWinner()
+        {
+            var expected = false;
+            var actual = new Game().HasWinner;
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
